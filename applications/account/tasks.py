5abe9e82ddb241781d8f621c801453ee5a8d6461
billal <karimovbillal20002@gmail.com>
2022-12-28 02:38:02 +0600

    added parser and beat

--- applications/account/tasks.py
+++ applications/account/tasks.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import get_user_model
+
+from applications.account.parser import get_url
 from config.celery import app
 from decouple import config
 from django.core.mail import send_mail
+
+User = get_user_model()
+URL = 'https://lenta.ru/rubrics/culture/music/'
 
 
 @app.task
@@ -21,4 +27,17 @@ def send_user_forgot_password_code(email, activation_code):
         f'Your link to change password {activation_code}',
         config('EMAIL_HOST_USER'),
         [email]
+    )
+
+
+email_list = [queryset.email for queryset in User.objects.filter(is_subscribed=True)]
+
+
+@app.task
+def send_spam():
+    send_mail(
+        'from music service "Yonko"',
+        f'{get_url(URL)}',
+        config('EMAIL_HOST_USER'),
+        email_list
     )

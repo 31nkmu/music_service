0480271c235c58968fa120b6da208f49c4bcc5ba
Rafiev <rafievvvv@gmail.com>
2023-01-02 19:52:15 +0600

    music crud, permissions and word with picture

--- applications/product/urls.py
+++ applications/product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from applications.product.views import MusicViewSet
+from applications.product.views import MusicViewSet, MusicPostConfirmAPIView
 
 router = DefaultRouter()
 router.register('', MusicViewSet)
 
 urlpatterns = [
+    path('post_confirm/<int:pk>/', MusicPostConfirmAPIView.as_view()),
     path('', include(router.urls)),
 ]

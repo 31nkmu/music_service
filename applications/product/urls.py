0abbe03480fb8939320d2d8b48ef60c761473ee0
Rafiev <rafievvvv@gmail.com>
2023-01-05 16:54:31 +0600

    recommendations added

--- applications/product/urls.py
+++ applications/product/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from django.views.decorators.cache import cache_page
-from applications.product.views import MusicViewSet, MusicPostConfirmAPIView, AlbumViewSet, HistoryAPIView
+from applications.product.views import MusicViewSet, MusicPostConfirmAPIView, AlbumViewSet, HistoryAPIView, BestAPIView
 
 router = DefaultRouter()
 router.register('album', AlbumViewSet)
 router.register('', MusicViewSet)
 
 urlpatterns = [
-    path('post_confirm/<int:pk>/', MusicPostConfirmAPIView.as_view()),
-    path('history/', cache_page(300)(HistoryAPIView.as_view())),
+    path('post_confirm/<int:pk>/', MusicPostConfirmAPIView.as_view(), name='post-confirm'),
+    path('history/', cache_page(300)(HistoryAPIView.as_view()), name='history-list'),
+    path('best/', BestAPIView.as_view(), name='best-list'),
     path('', include(router.urls)),
 ]

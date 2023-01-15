58463c77d19e0b1a3d07c522a729e25108a21e0e
billal <karimovbillal20002@gmail.com>
2023-01-15 23:16:57 +0600

    changed admin

--- applications/account/tasks.py
+++ applications/account/tasks.py
@@ -1,5 +1,3 @@
-import logging
-
 from django.contrib.auth import get_user_model
 
 from applications.account.parser import get_url
@@ -9,12 +7,10 @@ from django.core.mail import send_mail
 
 User = get_user_model()
 URL = 'https://lenta.ru/rubrics/culture/music/'
-logger = logging.getLogger('celery_logger')
 
 
 @app.task
 def send_user_activation_link(email, activation_code):
-    logger.info('send_activation_link')
     full_link = f'http://localhost:8000/api/v1/account/activate/{activation_code}'
     send_mail(
         'from music_service "Yonko"',
@@ -26,7 +22,6 @@ def send_user_activation_link(email, activation_code):
 
 @app.task
 def send_user_forgot_password_code(email, activation_code):
-    logger.info('send_user_forgot_password_code')
     send_mail(
         'from music_service "Yonko"',
         f'Your link to change password {activation_code}',
@@ -40,7 +35,6 @@ email_list = [queryset.email for queryset in User.objects.filter(is_subscribed=T
 
 @app.task
 def send_spam():
-    logger.info('send_spam')
     send_mail(
         'from music service "Yonko"',
         f'{get_url(URL)}',
